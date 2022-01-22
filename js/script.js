dda7fafde32f2701ff26bae1392f8d0ee54898e8
CHERIETNOURHENE <meriemcheriet44@gmail.com>
2022-01-22 09:29:19 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+function equal(a,b,c){
+    if(a==b&& b==c)
+    return true;
+   else return false;
+}
+function equal (a,b,c){
+    return(a == b && b == c)
+}
+console.log(equal("a","b","c"));
+console.log(equal("1","1","1"));
+//Convert Minute to second
+function second(minute){
+return minute*60;
+}
+console.log(second(1));
+
+function area(height,width){
+return height*width
+}
+console.log(area(10,5));	 //	50
+
+function compter(number){
+return number+1
+}
+console.log(compter(1));
+
+function football(a,b,c){
+    return(a*3+b*1)
+}
+console.log(football(1,2,1));	 ///	5pts
+
+function between(a){
+return a<200 && a>100
+}
+console.log(between(150));
+
+function convertHourMin(a,b){
+    return a*3600  +b*60
+    }
+    console.log(convertHourMin(1,30));
+    
+    function equal(a,b){
+        if(a==b)
+        return true;
+     else return false;
+    }
+    console.log(equal("1","1"));
+    console.log(equal("1","2"));
+
+    function strings(a,b){
+        if(a.length> b. length) console.log ("The	first	string	is	taller"	) 
+        else console.log("The	second	string	is	taller")
+}
+    strings("meriem","walideker");
+
+    
